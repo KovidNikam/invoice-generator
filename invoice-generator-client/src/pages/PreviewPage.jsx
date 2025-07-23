0b1bfabb0edb95bca8dd64f5945b0ce0af3321a1
veeralk <veeralkalmani@gmail.com>
2025-07-23 15:45:18 +0530

    preview page

--- invoice-generator-client/src/pages/PreviewPage.jsx
+++ invoice-generator-client/src/pages/PreviewPage.jsx
@@ -1,7 +1,33 @@
-const PreviewPage=()=>{
-    return(
-        <div>Preview Page</div>
-    )
-}
+import { templates } from "@assets/assets";
+import { AppContext } from "context/AppContext";
+import { useContext, useRef } from "react";
+
+const PreviewPage = () => {
+  const PreviewRef = useRef();
+  const { selectedTemplate } = useContext(AppContext);
+
+  return (
+    <div className="container-fluid d-flex flex-column p-3 min-vh-100">
+      <div className="d-flex flex-column align-items-center mb-4 gap-3">
+        <div className="d-flex gap-2 flex-wrap justify-content-center">
+          {templates.map(({ id, label }) => (
+            <button
+              key={id}
+              className={`btn btn-sm rounded-pill p-2 ${
+                selectedTemplate === id ? 'btn-warning' : 'btn-outline-secondary'
+              }`}
+            >
+              {label}
+            </button>
+          ))}
+        </div>
+
+        <div className="d-flex flex-wrap justify-content-center gap-2">
+          {/* Additional content here if needed */}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default PreviewPage;
