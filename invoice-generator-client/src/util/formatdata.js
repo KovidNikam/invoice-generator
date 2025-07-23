fd53933ee4cea7da1030982c93e4715271f4b909
veeralk <veeralkalmani@gmail.com>
2025-07-23 17:11:34 +0530

    formatdata page

---
+++ invoice-generator-client/src/util/formatdata.js
@@ -0,0 +1,55 @@
+export const formatInvoiceData = (InvoiceData) => { 
+    const {
+    title,
+    amount = {},
+    invoke = {},
+    account = {},
+    billing = {},
+    shipping = {},
+    tax = 0,
+    notes = "",
+    items = [],
+    logo = "",
+ } = InvoiceData || {};
+    
+    const currencySymbol = "$";
+    const subtotal = items.reduce((acc, item) => acc + (item.qty * item.amount), 0);
+    const taxAmount: number = subtotal * (tax / 100);
+    const total = subtotal + taxAmount;
+
+    return{
+        title
+        companyName: company.name,
+        companyAddress: company.address,
+        companyPhone: company.phone,
+        companyLogo: logo,
+
+        invoiceNumber: invoice.number,
+        invoiceDate: invoice.date,
+        paymentDate: invoice.dueDate,
+
+        accountName: account.name,
+        accountNumber: account.number,
+        accountTfscCode: account.ifsccode,
+
+        billingName: billing.name,
+        billingAddress: billing.address;
+        billingPhone: billing.phone;
+
+        shippingName: shipping.name,
+        shippingAddress: shipping.address,
+        shippingPhone: shipping.phone,
+
+        currencySymbol,
+        tax,
+        items,
+        notes,
+        subtotal,
+        taxAmount,
+        total
+
+
+    };
+
+
+};
