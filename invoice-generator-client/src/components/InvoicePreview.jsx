4437045c3a7324ada82ec258d5c229883f71e3fb
veeralk <veeralkalmani@gmail.com>
2025-07-24 14:32:20 +0530

    Template pages and util invoice page

--- invoice-generator-client/src/components/InvoicePreview.jsx
+++ invoice-generator-client/src/components/InvoicePreview.jsx
@@ -4,7 +4,7 @@ const InvoicePreview = forwardRef(({invoiceData, template},ref) => {
     const formattedData=formatInvoiceData(invoiceData);
     return(
         <div ref={ref} className="invoice-preview container px-2 py-2 overflow-x-auto">
-            render the pdf
+           <Template1 data={formattedData}/>
         </div>
     )
 });
